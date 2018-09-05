91d445a39370d00cde01d4ace327638a4a4d8122
Aniket Satarkar <aniketsatarkar9@gmail.com>
2018-09-05 18:39:44 +0530

    changed connection string for MongoDB

--- app.js
+++ app.js
@@ -3,33 +3,41 @@ const bodyParser = require('body-parser');
 const product    = require('./routes/product.route');
 
 // initialize MongoDB connection >>>
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// // let dev_db_url = `mongodb+srv://admin:bohemia@420@tododb-j3g8o.mongodb.net/test`;
-// let dev_db_url = `mongodb+srv://admin:bohemia%40420@tododb-j3g8o.mongodb.net/test?retryWrites=true`;
+// let dev_db_url = `mongodb+srv://admin:bohemia@420@tododb-j3g8o.mongodb.net/test`;
+let dev_db_url = `mongodb://admin:3akLrMe5u4lw5EhM@tododb-shard-00-00-j3g8o.mongodb.net:27017,tododb-shard-00-01-j3g8o.mongodb.net:27017,tododb-shard-00-02-j3g8o.mongodb.net:27017/test?ssl=true&replicaSet=tododb-shard-0&authSource=admin&retryWrites=true`;
 
-// let mongoDb = process.env.MONGODB_URI || dev_db_url;
+let mongoDb = process.env.MONGODB_URI || dev_db_url;
 
-// mongoose.connect(mongoDb, { uri_decode_auth: true, useNewUrlParser: true });
+mongoose.connect(mongoDb, {
+  uri_decode_auth: true, 
+  useNewUrlParser: true,
+  // auth: {
+  //   user: 'admin',
+  //   password: 'bohemia%420420'
+  // } 
 
-// mongoose.Promise = global.Promise;
-
-// let db = mongoose.connection;
-
-// db.on('error', console.error.bind(console, 'MongoDB connection error : '));
-
-
-const MongoClient = require('mongodb').MongoClient;
-const assert      = require('assert'); 
-
-let url = `mongodb+srv://admin:bohemia%40420@tododb-j3g8o.mongodb.net/test?retryWrites=true`;
-
-MongoClient.connect(url, function(err, db){
-  assert.equal(null, err);
-  console.log('Connection successfull!!!');
-  
-  db.close();
 });
+
+mongoose.Promise = global.Promise;
+
+let db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error : '));
+
+
+// const MongoClient = require('mongodb').MongoClient;
+// const assert      = require('assert'); 
+
+// let url = `mongodb+srv://admin:bohemia@420@tododb-j3g8o.mongodb.net/test?retryWrites=true`;
+
+// MongoClient.connect(url, { useNewUrlParser: true },function(err, db){
+//   assert.equal(null, err);
+//   console.log('Connection successfull!!!');
+  
+//   db.close();
+// });
 
 // initialize express app instance >>>
 const app = express();
